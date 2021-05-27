59500d1093b30197b36e6fb338ab5e5f62675286
Daniel Atwood <daniel@atwood.io>
2021-05-27 13:11:35 -0400

    Flatten ProcessConfig if statements

--- internal/plotConfig.go
+++ internal/plotConfig.go
@@ -39,28 +39,30 @@ type PlotConfig struct {
 }
 
 func (pc *PlotConfig) ProcessConfig() (newConfigLoaded bool) {
-	if fs, err := os.Lstat(pc.ConfigPath); err != nil {
+	fs, err := os.Lstat(pc.ConfigPath)
+	if err != nil {
 		log.Printf("Failed to open config file [%s]: %s\n", pc.ConfigPath, err)
-	} else {
-		if pc.LastMod != fs.ModTime() {
-			if f, err := os.Open(pc.ConfigPath); err != nil {
-				log.Printf("Failed to open config file [%s]: %s\n", pc.ConfigPath, err)
-			} else {
-				defer f.Close()
-				decoder := json.NewDecoder(f)
-				var newConfig Config
-				if err := decoder.Decode(&newConfig); err != nil {
-					log.Printf("Failed to process config file [%s], check your config file for mistake: %s\n", pc.ConfigPath, err)
-				} else {
-					pc.Lock.Lock()
-					pc.CurrentConfig = &newConfig
-					pc.Lock.Unlock()
-					log.Printf("New configuration loaded")
-					newConfigLoaded = true
-				}
-			}
-			pc.LastMod = fs.ModTime()
-		}
+		return
 	}
-	return
+	if pc.LastMod == fs.ModTime() {
+		return
+	}
+	f, err := os.Open(pc.ConfigPath)
+	if err != nil {
+		log.Printf("Failed to open config file [%s]: %s\n", pc.ConfigPath, err)
+		return
+	}
+	defer f.Close()
+	decoder := json.NewDecoder(f)
+	var newConfig Config
+	if err := decoder.Decode(&newConfig); err != nil {
+		log.Printf("Failed to process config file [%s], check your config file for mistake: %s\n", pc.ConfigPath, err)
+		return
+	}
+	pc.Lock.Lock()
+	pc.CurrentConfig = &newConfig
+	pc.Lock.Unlock()
+	log.Printf("New configuration loaded")
+	pc.LastMod = fs.ModTime()
+	return true
 }
